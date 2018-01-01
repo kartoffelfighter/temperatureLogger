b7b205139c40730d95c02bb59c9a2d5538d9dedd
marc <marc@marcfischer.org>
2018-01-01 17:04:21 +0100

    changed DHT Type

--- wifiSens/sourceCode/src/settings.h
+++ wifiSens/sourceCode/src/settings.h
@@ -12,23 +12,30 @@
 #define SERVER_PORT 1337
 #define SERVER_BASE_URL "/temperatureLogger/html/api.php"
 
-#define SENSID "0x002"
+#define SENSID "0x005"
 
 
-#define INTERVALL 10   // time in minuts between two measures
+#define INTERVALL 1   // time in minuts between two measures
 #define SAMPLE_RATE  5  // how often should measure be called while sampling
 #define SAMPLES 25 // Sampletime in Seconds before a value is valid
 
 
-#define DHT_TYPE DHT11  // define the DHT Sensor Type (Lib supports DHT11 / DHT22 / DHT21)
+#define DHT_TYPE "DHT22"  // define the DHT Sensor Type (Lib supports DHT11 / DHT22 / DHT21)
 
 #define OFFSET 273.15   // Offset for negative Temperatures (0K)
 // global integers
 
 long lastRenew = 0; // unix time for last renewed time
+long loadingSince = 0;  // millis since accu is loading
 float humidity, temperature, accu;
 float humidSample, tempSample, accuSample;
 
 int millisBeginnSampling = 0;
 
 DHT dht; // init dht sensor
+
+bool AccuLoading, AccuFull; // booleans for loading accu or accu full
+
+int lastMinute = 0;   // counter for status information
+int lastUpdateDay = 0;  // counter for updating time
+int lastMeasure = 0;  // counter for last measure
