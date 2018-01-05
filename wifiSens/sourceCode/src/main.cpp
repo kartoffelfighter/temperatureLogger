b46571b824e475afe9b332c57b25b1c5f02f4705
marc <marc@marcfischer.org>
2018-01-06 00:47:52 +0100

    sourced out loop, added librarys

--- wifiSens/sourceCode/src/main.cpp
+++ wifiSens/sourceCode/src/main.cpp
@@ -1,21 +1,25 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>    // http client
-#include <ArduinoJson.h>      // JSON Library
-#include <TimeLib.h>      // Time library
-#include <dht.h>  // DHT Sensor Library
-#include <ArduinoOTA.h>   // OTA Update library
+#include <ESP8266HTTPClient.h> // http client
+#include <ESP8266mDNS.h>       // mDNS package resolver for wifi_Server
+#include <ESP8266WebServer.h>  // Webserver for wifi_Server
+#include <SPI.h>               // SPI for SD-Card
+#include <SdFat.h>                // SD-Card
+#include <ArduinoJson.h>       // JSON Library
+#include <TimeLib.h>           // Time library
+#include <dht.h>               // DHT Sensor Library
+#include <ArduinoOTA.h>        // OTA Update library
 
 #include "settings.h"  //settings file
-#include "lang.h"   // language File
-#include "pins.h" // pin config
-#include "functions.h"  // functions file
-#include "loop.h" // the loop (cycle())
-
-#include "boot.h"     // startup relevant code
+#include "lang.h"      // language File
+#include "pins.h"      // pin config
+#include "functions.h" // functions file
+#include "boot.h" // startup relevant code
+#include "loop.h"      // the loop (cycle())
 #include "OTA.h"  // OTA Updates relevant Code
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     pinMode(ADC, INPUT);
     pinMode(accuLoading, INPUT);
@@ -25,8 +29,9 @@ void setup() {
     enableOTA();
 }
 
-void loop() {
-boot();   // call boot script to test system and establish wifi connection etc. can be recalled, if bool booted is set to false
-cycle();  // the "loop"
-ArduinoOTA.handle(); // OTA Update Handler
+void loop()
+{
+    boot();              // call boot script to test system and establish wifi connection etc. can be recalled, if bool booted is set to false
+    cycle();             // the "loop"
+    ArduinoOTA.handle(); // OTA Update Handler
 }
